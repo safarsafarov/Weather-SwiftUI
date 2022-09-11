8b9a92cd658f21ecf72d4ccc42bf62a52ecc6954
Safar Safarov <Safarov.safar@hotmail.com>
2022-09-11 12:03:27 +0500

    ✨

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Safar Safarov on 10/09/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
