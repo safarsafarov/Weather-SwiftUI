68e10abc9fe425fcbda1d23219c51a5a8bf02d95
Safar Safarov <Safarov.safar@hotmail.com>
2022-09-12 23:43:31 +0500

    ✨

---
+++ Weather/Weather/Utils/Shapes.swift
@@ -0,0 +1,20 @@
+//
+//  Shapes.swift
+//  Weather
+//
+//  Created by Safar Safarov on 12/09/22.
+//
+
+import SwiftUI
+
+struct Shapes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Shapes_Previews: PreviewProvider {
+    static var previews: some View {
+        Shapes()
+    }
+}
--- Weather/Weather/Views/Main/HomeView.swift
+++ Weather/Weather/Views/Main/HomeView.swift
@@ -9,55 +9,61 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
-            // MARK: Back
-            Color.background
-                .ignoresSafeArea()
-            
-            // MARK: Background Image
-            Image("Background")
-                .resizable()
-                .ignoresSafeArea()
-            
-            // MARK: House Image
-                .frame(maxHeight: .infinity, alignment: .top)
-                .padding(.top, 257)
-            
-            VStack(spacing: -10){
-                Text("Montreal")
-                    .font(.largeTitle)
+        NavigationView{
+            ZStack {
+                // MARK: Back
+                Color.background
+                    .ignoresSafeArea()
                 
-                VStack {
-                    Text("24°")
-                        .font(.system(size: 96, weight: .thin))
-                        .foregroundColor(.primary)
-                    +
-                    Text("\n ")
-                    +
-                    Text("Mostly Clear")
-                        .font(.title3.weight(.semibold))
-                        .foregroundColor(.secondary)
-                    Text("H:24°  L:18°")
-                        .font(.title3.weight(.semibold))
+                // MARK: Background Image
+                Image("Background")
+                    .resizable()
+                    .ignoresSafeArea()
+                
+                // MARK: House Image
+                    .frame(maxHeight: .infinity, alignment: .top)
+                    .padding(.top, 257)
+                
+                VStack(spacing: -10){
+                    Text("Montreal")
+                        .font(.largeTitle)
+                    
+                    VStack {
+                        Text("24°")
+                            .font(.system(size: 96, weight: .thin))
+                            .foregroundColor(.primary)
+                        +
+                        Text("\n ")
+                        +
+                        Text("Mostly Clear")
+                            .font(.title3.weight(.semibold))
+                            .foregroundColor(.secondary)
+                        Text("H:24°  L:18°")
+                            .font(.title3.weight(.semibold))
+                    }
+                    
+                    Spacer()
                 }
+                .padding(.top, 51)
                 
-                Spacer()
+                // MARK: Tab Bar
+                TabBar(action: {})
             }
-            .padding(.top, 51)
+            .navigationBarHidden(true)
         }
     }
     
     private var attributedString: AttributedString {
-        var string = AttributedString("19°" + "\n" + "Mostly Clear")
+        var string = AttributedString("19°" + "\n " + "Mostly Clear")
         
         if let temp = string.range(of: "19°"){
             string[temp].font = .system(size: 96, weight: .thin)
             string[temp].foregroundColor = .primary
         }
         
-        if let pipe = string.range(of: "|") {
+        if let pipe = string.range(of: " | ") {
             string[pipe].font = .title3.weight(.semibold)
-            string[weather].foregroundColor = .secondary
+            string[pipe].foregroundColor = .secondary
         }
         
         if let weather = string.range(of: "Mostly Clear") {
