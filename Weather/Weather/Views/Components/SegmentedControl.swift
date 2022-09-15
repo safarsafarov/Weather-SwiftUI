2c74e9419470ccb4fedcc3c21d38228c530d227c
Safar Safarov <Safarov.safar@hotmail.com>
2022-09-15 17:07:44 +0500

    ✨

--- Weather/Weather/Views/Components/SegmentedControl.swift
+++ Weather/Weather/Views/Components/SegmentedControl.swift
@@ -26,10 +26,27 @@ struct SegmentedControl: View {
                 } label: {
                     Text("Weekly Forecast")
                 }
-                .frame(maxWidth: 0, maxHeight: .infinity)
+                .frame(minWidth: 0, maxWidth: .infinity)
             }
             .font(.subheadline.weight(.semibold))
             .foregroundColor(.secondary)
+            
+            Divider()
+                .background(.white.opacity(0.5))
+                .blendMode(.overlay)
+                .shadow(color: .black.opacity(0.2), radius: 0, x: 0, y: 0)
+                .blendMode(.overlay)
+                .overlay{
+                    // MARK: Underline
+                    HStack {
+                        Divider()
+                            .frame(width: UIScreen.main.bounds.width / 2, height: 3)
+                            .background(Color.underline)
+                        .blendMode(.overlay)
+                    }
+                    .frame(maxWidth: .infinity, alignment: selection == 0 ? .leading : .trailing)
+                    .offset(y: -1)
+                }
         }
         .padding(.top, 25)
     }
